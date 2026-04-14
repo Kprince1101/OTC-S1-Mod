854004da1dc1f15715e2f7695f5d20c28396eea8
hdlmrell <mrelliottxbox@gmail.com>
2026-04-13 21:48:19 -0400

    Unify dispensary customer pricing formula and add enjoy-premium tipping

--- OverTheCounter/Core.cs
+++ OverTheCounter/Core.cs
@@ -23,7 +23,7 @@ using Il2CppFishNet.Object;
 using FishNet.Object;
 #endif
 
-[assembly: MelonInfo(typeof(OverTheCounter.Core), "OverTheCounter", "2.0.9", "hdlmrell", null)]
+[assembly: MelonInfo(typeof(OverTheCounter.Core), "OverTheCounter", "2.0.10", "hdlmrell", null)]
 [assembly: MelonGame("TVGS", "Schedule I")]
 [assembly: MelonOptionalDependencies("SteamNetworkLib")]
 [assembly: HarmonyDontPatchAll]
